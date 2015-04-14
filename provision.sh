db160e0a57c33dbe810c43de639011ffda50671f
Cody Piersall <codypi@ou.edu>
2015-04-14 14:13:13 -0500

    Add ubuntu to plugins.

--- provision.sh
+++ provision.sh
@@ -277,7 +277,7 @@ ZSH_THEME="robbyrussell"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git mercurial python vi-mode vim virtualenvwrapper)
+plugins=(git mercurial python vi-mode vim virtualenvwrapper ubuntu)
 
 # User configuration
 
